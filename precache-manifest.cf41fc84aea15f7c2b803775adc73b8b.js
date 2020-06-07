0398c268940c35d410b582b924230e93458429f7
skukreja <skukreja434@gmail.com>
2020-06-07 23:37:33 +0530

    Updates

---
+++ precache-manifest.cf41fc84aea15f7c2b803775adc73b8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cb5afbc330cdfcd4b4f1c0224b25e4",
+    "url": "/dialogues/index.html"
+  },
+  {
+    "revision": "3623a8abc0d5a366ddbe",
+    "url": "/dialogues/static/css/main.41aff5a5.chunk.css"
+  },
+  {
+    "revision": "affaaaf52a26f7b1cd6a",
+    "url": "/dialogues/static/js/2.1fb4b1ba.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/dialogues/static/js/2.1fb4b1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3623a8abc0d5a366ddbe",
+    "url": "/dialogues/static/js/main.7b379934.chunk.js"
+  },
+  {
+    "revision": "48269cc17c033cf388df",
+    "url": "/dialogues/static/js/runtime-main.0f0b3e04.js"
+  },
+  {
+    "revision": "c37cc212fbdeef64d29202cd9b2de80f",
+    "url": "/dialogues/static/media/bg.c37cc212.png"
+  }
+]);
