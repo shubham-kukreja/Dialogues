e624ab95b8c641150f279faace6a3c69c990d20f
skukreja <skukreja434@gmail.com>
2020-06-07 23:46:55 +0530

    api-key-removed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import { Container } from "@material-ui/core";
 import * as firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDDpc2caXDRW_Lwpm8Qj-qfZONGvwwSgPE",
-  authDomain: "coronacountindia.firebaseapp.com",
-  databaseURL: "https://coronacountindia.firebaseio.com",
-  projectId: "coronacountindia",
-  storageBucket: "coronacountindia.appspot.com",
-  messagingSenderId: "444376998366",
-  appId: "1:444376998366:web:5caa794e24b40d4274fd4f",
+  //your api key
 };
 firebase.initializeApp(firebaseConfig);
 
